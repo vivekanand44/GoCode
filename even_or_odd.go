e49061d4ccb788ebb162ba128a9311f4af6fa4ad
vivek <khyade.vivekanand@gmail.com>
2023-07-28 10:44:06 +0530

    Update even_or_odd.go
    Even or Odd

--- even_or_odd.go
+++ even_or_odd.go
@@ -5,10 +5,10 @@ import "fmt"
 func main() {
 	for i := 0; i <= 20; i++ {
 		if i%2 == 0 {
-			fmt.Println("Even", i)
+			fmt.Println("The number is Even =", i)
 
 		} else {
-			fmt.Println("Odd", i)
+			fmt.Println("The number is Odd =", i)
 		}
 	}
 }
